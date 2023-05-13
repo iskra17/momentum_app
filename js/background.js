26aa32761b706d83fd05db8c6c5dae5d3b8b6e76
iskra17 <rayanami@naver.com>
2023-05-13 16:18:05 +0900

    배경 이미지 로딩 개선

--- js/background.js
+++ js/background.js
@@ -14,21 +14,28 @@ const images = [
   "12.jpg",
 ]
 
-
-
 const bgDiv = document.createElement("div")
 bgDiv.classList.add("background")
 document.body.appendChild(bgDiv)
 
 function changeBackground() {
-  const chosenImage= images[Math.floor(Math.random() * images.length)]
-  bgDiv.style.background = `url(img/${chosenImage}) no-repeat center center`
-  bgDiv.style.backgroundSize = "cover"
+  const chosenImage = images[Math.floor(Math.random() * images.length)]
+  const url = `url(img/${chosenImage})`
+
+  // 새로운 Image 객체를 생성
+  let img = new Image()
+
+  // 이미지 로딩이 끝나면 실행할 콜백 함수를 설정
+  img.onload = function () {
+    // 이미지 로딩이 끝나면 bgDiv의 배경 이미지를 변경
+    bgDiv.style.background = `${url} no-repeat center center`
+    bgDiv.style.backgroundSize = "cover"
+  }
+
+  // 이미지 로딩 시작
+  img.src = `img/${chosenImage}`
 }
 
 changeBackground()
 
-setInterval(changeBackground,10000)
-
-
-
+setInterval(changeBackground, 10000)
